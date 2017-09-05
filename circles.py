ec477390a5770a3b65f231addee664f709ffa28c
Osher Lerner <osherler@gmail.com>
2017-09-05 00:02:42 -0700

    added collisions

--- circles.py
+++ circles.py
@@ -1,16 +1,38 @@
-# 
+# Code largely based on Jen Selby's example code circles.py
 
 import Tkinter # built-in Python graphics library
 import random, time
+import numpy as np
 from abc import ABCMeta, abstractmethod, abstractproperty
 
-class GameObject(Object):
+class GameObject(object):
     __metaclass__ = ABCMeta
 
-    def update(self):
-        '''Update current location by speed.'''
-        self.x += self.x_speed
-        self.y += self.y_speed
+    max_speed = 5
+    min_size, max_size = 10, 50
+
+    def __init__(self, x, y, x_speed=None, y_speed=None, size=None, color=None):
+        '''Create a new circle at the given x,y point with a random speed, color, and size.'''
+        self.x = x
+        self.y = y
+        if x_speed == None:
+            self.x_speed = random.randint(-self.max_speed, self.max_speed)
+        else:
+            self.x_speed = x_speed
+        if y_speed == None:
+            self.y_speed = random.randint(-self.max_speed, self.max_speed)
+        else:
+            self.y_speed = y_speed
+        # this creates a random hex string between #000000 and #ffffff
+        # we draw it with an outline, so we'll be able to see it on a white background regardless
+        if color == None:
+            self.color = '#{0:0>6x}'.format(random.randint(00,16**6))
+        else:
+            self.color = color
+        if size == None:
+            self.size = random.randint(self.min_size, self.max_size)
+        else:
+            self.size = None
 
     @abstractmethod
     def draw(self, canvas):
@@ -36,83 +58,54 @@ class GameObject(Object):
     def y_speed(self):
         pass
 
-class Circle(GameObject):
-    min_size, max_size = 10, 50
-    def __init__(self, x, y, x_speed=None, y_speed=None, size=None, color=None):
-        '''Create a new circle at the given x,y point with a random speed, color, and size.'''
-        self.x = x
-        self.y = y
-        if x_speed == None:
-            self.x_speed = random.randint(-5,5)
-        else:
-            self.x_speed = x_speed
-        if y_speed == None:
-            self.y_speed = random.randint(-5,5)
-        else:
-            self.y_speed = y_speed
-        # this creates a random hex string between #000000 and #ffffff
-        # we draw it with an outline, so we'll be able to see it on a white background regardless
-        if color == None:
-            self.color = '#{0:0>6x}'.format(random.randint(00,16**6))
-        else:
-            self.color = color
-        if size == None:
-            self.size = random.randint(10,50)
-        else:
-            self.size = None
-
     def update(self):
         '''Update current location by speed.'''
-
-        if random.random() > 0.5:
-            self.checkWallsBounce()
-        else:
-            self.checkWallsTesselate()
-
         self.x += self.x_speed
         self.y += self.y_speed
 
-    
+class Circle(GameObject):
+    __metaclass__ = ABCMeta
+    min_size, max_size = 7, 100
+    max_speed = 15
+    x,y,size,x_speed,y_speed=0,0,0,0,0
+    max_size = 50
+
     def draw(self, canvas):
         '''Draw self on the canvas.'''
-        canvas.create_oval(self.x - self.size/2, self.y - self.size/2, self.x + self.size/2, self.y + self.size/2,
-                           fill=self.color, outline="black")
+        canvas.create_oval(self.x - self.size/2, self.y - self.size/2, self.x + self.size/2, self.y + self.size/2, fill=self.color, outline="black")
 
-class Position():
-    def __init__(self, x, y=None):
-        if y == None:
-            if type(x) == tuple or type(x) == list:
-                self.x = x[0]
-                self.y = x[1]
-        else:
-            self.x = x
-            self.y = y
-
-class Canvas():
-    def __init__(self, width=400, height=400, bounce=False, tesselate=False, collide=True):
-        root = Tkinter.Tk()
-        self.canvas = Tkinter.Canvas(root, width=width, height=height)
+class Canvas(object):
+    def __init__(self, width=400, height=400, bounce=False, tesselate=False, collide=True, delay=33):
+        self.root = Tkinter.Tk()
+        self.canvas = Tkinter.Canvas(self.root, width=width, height=height)
         self.canvas.pack()
 
         self.width = width
         self.height = height
+        self.delay = delay
 
-        self.collide=collide
+        self.collide = collide
         if tesselate:
             self.tesselate = True
             self.bounce = False
         else:
+            self.tesselate = False
             self.bounce = bounce
 
         self.reset()
 
-    def draw(self):
+    def reset(self, event=None):
+        '''Clear all game objects.'''
+        self.game_objects = []
+
+    def draw(self, other=None):
         '''Clear the canvas, have all game objects update and redraw, then set up the next draw.'''
         self.canvas.delete(Tkinter.ALL)
 
-        for game_object in self.game_objects:
+        for i in range(len(self.game_objects)):
+            game_object = self.game_objects[i]
             if self.collide:
-                for game_object_2 in self.game_objects:
+                for game_object_2 in self.game_objects[i+1:]: # index+1 and on ensures no duplicates or same object
                     self.checkCollision(game_object, game_object_2)
 
             if self.bounce:
@@ -123,14 +116,17 @@ class Canvas():
             game_object.update()
             game_object.draw(self.canvas)
 
-        delay = 33 # milliseconds, so about 30 frames per second
-        self.canvas.after(delay, self.draw, self.canvas) # call this draw function with the canvas argument again after the delay
+        self.canvas.after(self.delay, self.draw, self.canvas) # call this draw function with the canvas argument again after the delay
+
+    def run(self):
+        self.draw()
+        self.root.mainloop() # keep the window open
 
     def checkBounce(self, game_object):
         if game_object.x >= self.width or game_object.x <= 0:
-            self.x_speed = -game_object.x_speed
+            game_object.x_speed = -game_object.x_speed
         if game_object.y >= self.height or game_object.y <= 0:
-            self.y_speed = -game_object.y_speed
+            game_object.y_speed = -game_object.y_speed
 
     def checkTesselate(self, game_object):
         if game_object.x >= self.width:
@@ -146,8 +142,23 @@ class Canvas():
         distance = np.sqrt((game_object_1.x-game_object_2.x)**2 + (game_object_1.y-game_object_2.y)**2)
         min_distance = game_object_1.size + game_object_2.size
         if distance <= min_distance:
-            pass
+            game_object_1.mass = game_object_1.size**2
+            game_object_1.direction = np.arctan2(game_object_1.y_speed, game_object_1.x_speed)
+            game_object_1.speed = np.sqrt(game_object_1.x_speed**2 + game_object_1.y_speed**2)
+            game_object_2.mass = game_object_2.size**2
+            game_object_2.direction = np.arctan2(game_object_2.y_speed, game_object_2.x_speed)
+            game_object_2.speed = np.sqrt(game_object_2.x_speed**2 + game_object_2.y_speed**2)
+            collisionAngle = np.arctan2(game_object_1.y-game_object_2.y, game_object_1.x-game_object_2.x)
+            
+            a1 = (game_object_1.speed*np.cos(game_object_1.direction-collisionAngle)*(game_object_1.mass-game_object_2.mass)+2*game_object_2.mass*game_object_2.speed*np.cos(game_object_2.direction-collisionAngle))/(game_object_1.mass+game_object_2.mass)
+            b1 = game_object_1.speed*np.sin(game_object_1.direction-collisionAngle)
+            a2 = (game_object_2.speed*np.cos(game_object_2.direction-collisionAngle)*(game_object_2.mass-game_object_1.mass)+2*game_object_1.mass*game_object_1.speed*np.cos(game_object_1.direction-collisionAngle))/(game_object_2.mass+game_object_1.mass)
+            b2 = game_object_2.speed*np.sin(game_object_2.direction-collisionAngle)
 
+            game_object_1.x_speed = a1*np.cos(collisionAngle)+b1*np.cos(collisionAngle+90)
+            game_object_2.x_speed = a2*np.cos(collisionAngle)+b2*np.cos(collisionAngle+90)
+            game_object_1.y_speed = a1*np.sin(collisionAngle)+b1*np.sin(collisionAngle+90)
+            game_object_2.y_speed = a2*np.sin(collisionAngle)+b2*np.sin(collisionAngle+90)
 
     def addGameObject(self, game_object):
         if isinstance(game_object, GameObject):
@@ -161,9 +172,6 @@ class Canvas():
         '''Add a new circle where the user clicked.'''
         self.addCircleAtPos(event.x, event.y)
 
-    def reset(event=None):
-        '''Clear all game objects.'''
-        self.game_objects = []
 
 # this is a standard Python thing: definitions go above, and any code that will actually
 # run should go into the __main__ section. This way, if someone imports the file because
@@ -171,21 +179,16 @@ class Canvas():
 # automatically
 if __name__ == '__main__':
 
-    # create the graphics root and a 400x400 canvas
-    
+    canvas = Canvas(bounce=True)
 
     for i in range(10):
-        x, y = random.randint(max_size, width-max_size), random.randint(max_size, height-max_size)
-        addCircle(Position(x, y))
-    # circle_b = Circle(100, 150)
-    # time.sleep(500)
+        x, y = random.randint(Circle.max_size, canvas.width-Circle.max_size), random.randint(Circle.max_size, canvas.height-Circle.max_size)
+        canvas.addCircleAtPos(x, y)
     
     # if the user presses a key or the mouse, call our handlers
-    root.bind('<Key-r>', reset)
-    root.bind('<Button-1>', addCircle)
+    canvas.root.bind('<Key-r>', canvas.reset)
+    canvas.root.bind('<Button-1>', canvas.addCircleAtClick)
 
     # start the draw loop
-    draw(canvas)
-
-    root.mainloop() # keep the window open
+    canvas.run()
 
